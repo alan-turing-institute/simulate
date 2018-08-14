45d46845b5a51a47efa266ba574ba9031a0aa1c0
masonlr <l.mason@imperial.ac.uk>
2018-08-14 15:25:14 +0100

    fix: Update purge script for simulate (rename gateway)

--- purge.sh
+++ purge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "INFO: removing gateway_database"
-docker volume rm gateway_database
+echo "INFO: removing simulate_database"
+docker volume rm simulate_database
 
-echo "INFO: removing gateway_database_auth"
-docker volume rm gateway_database_auth
+echo "INFO: removing simulate_database_auth"
+docker volume rm simulate_database_auth
